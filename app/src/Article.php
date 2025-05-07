88d17d57cab7e66f3ed4f75cb477e387dea0bfb8
mandy <mandyhale10@gmail.com>
2025-05-07 14:23:12 +1200

    use can_be_root private static variable to prevent articles from being created at root level

--- app/src/Article.php
+++ app/src/Article.php
@@ -6,5 +6,9 @@ use Page;
 
 class ArticlePage extends Page
 {
-    //
+    /**
+     * use on page classes that you don't want to be created at the root level
+     * @var bool
+     */
+    private static $can_be_root = false;
 }
