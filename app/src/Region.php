f12df544ee1e22c4efd7e0cd4839e69255005a99
mandy <mandyhale10@gmail.com>
2025-05-08 19:47:03 +1200

    add versioning for region data

--- app/src/Region.php
+++ app/src/Region.php
@@ -2,27 +2,56 @@
 
 namespace SilverStripe\Example;
 
+use PHPUnit\Runner\Version;
 use SilverStripe\Assets\Image;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\TextareaField;
 use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Versioned\Versioned;
 
 class Region extends DataObject
 {
+    /**
+     * Config
+     * Applies the version data extension to support versioning of records
+     */
+    private static $extensions = [
+        Versioned::class,
+    ];
+
+    /**
+     * config
+     */
     private static $class_description = 'A region';
 
+    /**
+     * config
+     */
     private static $table_name = 'Region';
 
+    /**
+     * db properties
+     */
     private static $db = [
         'Title' => 'Varchar',
         'Description' => 'Text',
     ];
 
+    /**
+     * relations
+     */
     private static $has_one = [
         'Photo' => Image::class,
         'RegionPage' => RegionPage::class,
+    ];
+
+    /**
+     * ownership binding
+     */
+    private static $owns = [
+        'Photo',
     ];
 
     public function getCMSFields()
