f54f72dc35b1542a94c30b53d2d119a1cda006c1
mandy <mandyhale10@gmail.com>
2025-05-08 19:28:22 +1200

    Add a gridfield for regions on region page in CMS

--- app/src/RegionPage.php
+++ app/src/RegionPage.php
@@ -3,6 +3,8 @@
 namespace SilverStripe\Example;
 
 use Page;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 
 class RegionPage extends Page
 {
@@ -12,4 +14,17 @@ class RegionPage extends Page
     private static $has_many = [
         'Regions' => Region::class,
     ];
+
+    public function getCMSFields()
+    {
+
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldToTab(('Root.Regions'), GridField::create(
+            'Regions', // the name of the gridfield - needed if you ever want to make updates to your GridField after it's been added to the FieldList
+            'Regions on this page', //the user friendly title for the gridfield
+            $this->Regions(), // get the data to dispaly via the relationship on this class
+            GridFieldConfig_RecordEditor::create() //It creates a object that contains a number of GridFieldComponent objects, which provide various UI tools to the grid which you can add or remove inside the create parenthesis. By default the Record_Editor grid field provides good basic UI for managing data objects.
+        ));
+    }
 }
